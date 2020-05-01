ddb7bfaa7d7c87ac7ec9a7ae020c7c6e61aa547a
FrancescoTerrosi <francesco.terrosi1@gmail.com>
2020-05-01 20:08:17 +0200

    ue

---
+++ CHIP8/CHIP8/chip8.cpp
@@ -0,0 +1,80 @@
+#include "chip8.h"
+
+unsigned short opcode;		// 2 byte per le operazioni macchina
+
+unsigned char memory[4096];		// 4k per la memoria
+
+unsigned char V[16];		// 16 byte per i registri della CPU
+
+unsigned short I;			//	Index Register
+unsigned short pc;			// Program Counter
+
+unsigned char gfx[64 * 32];  // Graphics
+
+unsigned char delay_timer;
+unsigned char sound_timer;
+
+unsigned short stack[16];
+unsigned short sp;
+
+void Chip8::initialize()
+{
+	pc = 0x200;
+	opcode = 0;
+	I = 0;
+	sp = 0;
+
+	memset(memory, 0, 4096);
+	memset(V, 0, 16);
+	memset(gfx, 0, 64 * 32);
+	memset(stack, 0, 16);
+
+	for (int i = 0; i < 80; ++i)
+	{
+		memory[i] = chip8_fontset[i];
+	}
+
+
+
+	return;
+}
+
+void Chip8::emulateCycle()
+{
+	// FETCH OPCODE
+
+	// Devo combinare memory[pc] e memory[pc+1] --> ne leggo uno, shifto di 1 byte e faccio l'or con l'altro bit
+
+	opcode = memory[pc] << 8 | memory[pc + 1];
+
+	// DECODE OPCODE
+
+	switch (opcode & 0xF000)
+	{
+		// DECODE DEGLI OPCODE
+
+		case 0xA000:
+			// EXECUTE
+			I = opcode & 0x0FFF;
+			pc += 2;
+			break;
+
+		default:
+			printf("Opcode Error!!\n Code: 0x%X\n", opcode);
+
+	}
+
+	if (delay_timer > 0)
+	{
+		--delay_timer;
+	}
+
+	if (sound_timer > 0)
+	{
+		if (sound_timer == 1)
+		{
+			printf("BEEP");
+		}
+		--sound_timer;
+	}
+}
---
+++ CHIP8/CHIP8/game.cpp
@@ -0,0 +1,32 @@
+#include "chip8.cpp"
+
+Chip8 myChip8;
+
+int main(int argc, char** argv)
+{
+	//setupGraphics();
+	//setupInput();
+
+	//myChip8.initialize();
+	//myChip8.loadGame("pong");
+
+	while (1)
+	{
+		//emulate one cycle
+		
+		//myChip8.emulateCycle();
+
+		// if draw flag, update screen
+		if (myChip8.drawFlag)
+		{
+			//drawGraphics();
+		}
+
+		// store keys press state (press-release)
+
+		//myChip8.setKeys();
+
+	}
+
+	return 0;
+}
