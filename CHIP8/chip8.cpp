e95493ef6dd2f1315960752f8306437333318eb0
FrancescoTerrosi <francesco.terrosi1@gmail.com>
2020-05-02 18:49:34 +0200

    implementing cpu instructions

--- CHIP8/chip8.cpp
+++ CHIP8/chip8.cpp
@@ -94,6 +94,116 @@ void Chip8::emulateCycle()
 			pc = opcode & 0x0FFF;
 			break;
 
+		// 0x3XNN	if V[X] == NN, skip next instruction
+		case 0x3000:
+			if (V[opcode & 0x0F00] == (opcode & 0x00FF))
+			{
+				pc += 4;
+			}
+			break;
+
+			// 0x4XNN	if V[X] != NN, skip next instruction
+		case 0x4000:
+			if (V[opcode & 0x0F00] != (opcode & 0x00FF))
+			{
+				pc += 4;
+			}
+			break;
+
+		// 0x5XY0	if V[X] == V[Y], skip next instruction
+		case 0x5000:
+			if (V[opcode & 0x0F00] == V[opcode & 0x00F0])
+			{
+				pc += 4;
+			}
+			break;
+
+		// 0x6XNN	V[X] = NN
+		case 0x6000:
+			V[opcode & 0x0F00] = (opcode & 0x00FF);
+			pc += 2;
+			break;
+
+		// 0x7XNN	V[X] += NN			???? CARRY FLAG IS NOT CHANGED ?????
+		case 0x7000:
+			V[opcode & 0x0F00] += (opcode & 0x0FF);
+			pc += 2;
+			break;
+
+		// Handling multiple 0x8 cases
+		case 0x8000:
+			switch (opcode & 0x000F)
+			{
+				// 0x8XY0	V[X] = V[Y]
+				case 0x0000:
+					V[opcode & 0x0F00] = V[opcode & 0x00F0];
+					pc += 2;
+					break;
+				
+				// 0x8XY1	V[X] = V[X] | V[Y]
+				case 0x0001:
+					V[opcode & 0x0F00] = (V[opcode & 0x0F00] | V[opcode & 0x00F0]);
+					pc += 2;
+					break;
+					
+				// 0x8XY2	V[X] = V[X] & V[Y]
+				case 0x0002:
+					V[opcode & 0x0F00] = (V[opcode & 0x0F00] & V[opcode & 0x00F0]);
+					pc += 2;
+					break;
+
+					// 0x8XY3	V[X] = V[X] ^ V[Y]
+				case 0x0003:
+					V[opcode & 0x0F00] = (V[opcode & 0x0F00] ^ V[opcode & 0x00F0]);
+					pc += 2;
+					break;
+
+				/*
+				
+				// 0x8XY4	V[X] = V[X] + V[Y]		V[F] = 1 if carry, else 0
+				case 0x0004:
+					V[opcode & 0x0F00] = (V[opcode & 0x0F00] + V[opcode & 0x00F0]);
+					pc += 2;
+					break;
+					
+				// 0x8XY5	V[X] = V[X] - V[Y]		V[F] = 0 if borrow, else 1
+				case 0x0005:
+					V[opcode & 0x0F00] = (V[opcode & 0x0F00] - V[opcode & 0x00F0]);
+					pc += 2;
+					break;
+
+					*/
+
+				// 0x8XY6	V[F] = bit meno significativo di V[X] and V[X] >> 1
+				case 0x0006:
+					V[0x000F] = V[opcode & 0x0F00] & 0x0001;
+					V[opcode & 0x0F00] >> 1;
+					pc += 2;
+					break;
+
+				/*
+
+				// 0x8XY7	V[X] = V[Y] - V[X]		V[F] = 0 if borrow, else 1
+				case 0x0007:
+					V[opcode & 0x0F00] = (V[opcode & 0x00F0] - V[opcode & 0x0F00]);
+					pc += 2;
+					break;
+
+				*/
+
+				// 0x8XYE	V[F] = bit meno significativo di V[X] and V[X] << 1
+				case 0x000E:
+					V[0x000F] = V[opcode & 0x0F00] & 0x0001;
+					V[opcode & 0x0F00] << 1;
+					pc += 2;
+					break;
+
+
+				default:
+					printf("Unknown opcode 0x%X\n", opcode);
+					fflush(stdout);
+			}
+			break;
 
 		// 0xANNN -->  set I to address NNN
 		case 0xA000:
@@ -108,7 +218,7 @@ void Chip8::emulateCycle()
 
 		// 0xCXNN -->  V[X] = rand() % NN         0 <= rand <= 255
 		case 0xC000:
-			V[opcode & 0xF00] = (opcode & 0xFF) & (rand() % 256);
+			V[opcode & 0x0F00] = (opcode & 0x00FF) & (rand() % 256);
 			pc += 2;
 			break;
 
